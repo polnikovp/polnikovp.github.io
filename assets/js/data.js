730568729454435d32b550433c051e11a3378589
RDPROST <52405648+RDPROST@users.noreply.github.com>
2022-04-22 21:03:45 +0300

    Add Lecture 10

--- assets/js/data.js
+++ assets/js/data.js
@@ -140,6 +140,558 @@ if (localStorage.getItem('aData') !== null) {
                 }
             ],
             results: 0
+        },
+        {
+            title: "Лекция 3. Структура и состав персонального компьютера",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 4. Блоки питания Система охлаждения",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 5. Назначение и классификация внешних запоминающих устройств",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 6. Накопители на жестких магнитных дисках НЖМД принцип работы",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 7. Логическая структура жесткого диска Форматирование магнитных",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 8. Флеш память и накопители SSD Принцип работы, основные технические",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 9. Приводы CD R RW, DVD R RW принципы работы, конструкция",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
+        },
+        {
+            title: "Лекция 10. Видеокарта Режимы работы, Глубина цвета и разрешения Характеристики",
+            poll: [
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1s",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                },
+                {
+                    name: "Quote",
+                    answer: [
+                        {
+                            title: "Name 1",
+                            right: false
+                        },
+                        {
+                            title: "Name 2",
+                            right: false
+                        },
+                        {
+                            title: "Name 3",
+                            right: true
+                        },
+                        {
+                            title: "Name 4",
+                            right: false
+                        }
+                    ]
+                }
+            ],
+            results: 0
         }
     ]
     localStorage.setItem('aData', JSON.stringify(aData));
